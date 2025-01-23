08beb0135ffdc6af7f150f5001f635f4c76fb6a7
Sanish <sanishpagui162@gmail.com>
2025-01-23 21:28:49 +0530

    ez

--- src/app/component/Navbar.js
+++ src/app/component/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <div className='fixed w-full h-32 flex justify-center items-center'>
         <div>
             <ul className='flex space-x-7 text-2xl '>
-                <Link href={'/Home'}><motion.li whileHover={{}} className='hover:underline'>Home</motion.li></Link>
+                <Link href={'/'}><motion.li whileHover={{}} className='hover:underline'>Home</motion.li></Link>
                 {/* <Link href={'#'}><motion.li whileHover={{ }} className='hover:underline'>Services</motion.li></Link> */}
                 <Link href={'/About'}><motion.li whileHover={{ }} className='hover:underline'>About</motion.li></Link>
                 <Link href={'#'}><motion.li whileHover={{ }} className='hover:underline'>Contact</motion.li></Link>
