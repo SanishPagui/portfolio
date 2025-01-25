cb0717d6fc9e4e7eaf046edc07d012103f78001c
Sanish <sanishpagui162@gmail.com>
2025-01-25 21:53:03 +0530

    deploying

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-'use client'
+'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './Home/page';
 import LocomotiveScroll from 'locomotive-scroll';
 import Cursor from './component/Cursor';
 
+export default function Page() {
 
-export default function page() {
-
-    const locomotiveScroll = new LocomotiveScroll();
+    useEffect(() => {
+        const locomotiveScroll = new LocomotiveScroll();
+        return () => {
+            locomotiveScroll.destroy(); 
+        };
+    }, []);
 
     return (
         <div className='w-full h-full view'>
